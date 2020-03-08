2031009ea5109de6681accd1a5fa8b9d5ce94b17
Tabassum Hassan <tiram819@gmail.com>
2020-03-08 21:06:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return "#{array[0]}"
   elsif array.length == 2
-    return array.join( && )
+    return array.join(" and " ) 
   elsif array.length >= 3
     item = "and #{array[-1]}"
     array.pop
